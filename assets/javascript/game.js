f6db8c0eaf8101d80bc5e2f7b322a526dbe2859d
SchneiderMan <jschneid94@gmail.com>
2018-06-15 11:08:44 -0700

    Finished game, need to fix the list of guesses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,21 +23,37 @@ document.onkeyup = function (event) {
 
     // Store the player's guess
     var playerGuess = event.key;
+    console.log(playerGuess);
     
     // Store the computer's guess
     var compGuess = alphaArray[Math.floor(Math.random() * alphaArray.length)]
     console.log(compGuess);
 
     // Compare the user and computer's guess
-        // If the user guesses correctly, increase the win score by 1.
-        // If the user guesses incorrectly, and decrease the guesses remaining by 1.
-
+    // If the user guesses correctly, increase the win score by 1.
+    if (playerGuess === compGuess) {
+        wins++;
+    }
+     // If the user guesses incorrectly, and decrease the guesses remaining by 1.
+    else {
+        remainingGuess--;
+    }
 
     // Call the function to dipslay each guess the player has made
     guessList(playerGuess);
 
     // If the guesses remaining score reaches zero...
+    if (remainingGuess === 0) {
         // Increase the loss score by 1
+        losses++;
         // Reset the guesses remaing score to 10.
+        remainingGuess = 10;
+    }    
+
+    // Display the scores to refresh every time the user presses a key.
+    document.getElementById("winCount").innerHTML = "Wins: " + wins;
+    document.getElementById("lossCount").innerHTML = "Losses: " + losses;
+    document.getElementById("guessRemaining").innerHTML = "Guesses left: " + remainingGuess;
+    document.getElementById("guessCount").innerHTML = "Your guesses so far: " + playerGuess;
     
 }
